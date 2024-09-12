f00bbf616d8805302d569bce0db844902f79da5e
Siddhi2102 <agrawalsiddhi2102@gmail.com>
2024-09-12 21:04:50 +0530

    feat: add comment functionality for blog posts
    - Implemented route to post comments linked to specific blogs
    - Added backend logic to create comments with content, blogId, and userId
    - Redirect user to blog post after submitting a comment

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
- const path =require("path");
+require("dotenv").config();
+
+const path =require("path");
  const express=require('express');
  const mongoose=require("mongoose");
 const cookieParser= require('cookie-parser');
@@ -14,9 +16,9 @@ const blogRoute=require('./routes/blog');
 const { checkForAuthenticationCoookie } = require("./middlewares/authentication");
 
  const app=express();
- const PORT=8000;
+ const PORT=process.env.PORT ||8000;
 
- mongoose.connect('mongodb+srv://agrawalsiddhi2102:FJhRXLydS8ECGf52@blog.xoq1z.mongodb.net/?retryWrites=true&w=majority&appName=Blog').then((e)=>console.log("MongoDB Connected"));
+ mongoose.connect(process.env.MONGO_URL).then((e)=>console.log("MongoDB Connected"));
 
  app.set("view engine","ejs");
  app.set("views",path.resolve("./views"));
